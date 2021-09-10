4e6a9a4262016b30fba8a7a824e56cbd607a90ad
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-10 20:48:45 +0530

    added script for basic dropdown item selection

--- dropdown.js
+++ dropdown.js
@@ -1,18 +1,22 @@
-const langDropBtn = document.querySelector(".js-lang-dropbtn");
-const themeDropBtn = document.querySelector(".js-theme-dropbtn");
-
-themeDropBtn.addEventListener("click", function () {
-  document.getElementById("themeDropdown").classList.toggle("show");
-});
-themeDropBtn.addEventListener("blur", function () {
-  let a = document.querySelector(".js-theme-dropdown-content");
-  a.classList.remove("show");
-});
-
-langDropBtn.addEventListener("click", function () {
-  document.getElementById("langDropdown").classList.toggle("show");
-});
-langDropBtn.addEventListener("blur", function () {
-  let a = document.querySelector(".js-lang-dropdown-content");
-  a.classList.remove("show");
-});
+export function HandleThemeDropBtnClickEvent(dropContent) {
+  let items = dropContent.getElementsByClassName("theme-item");
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active-theme");
+      current[0].className = current[0].className.replace(" active-theme", "");
+      this.className += " active-theme";
+      dropContent.classList.remove("show");
+    });
+  }
+}
+export function HandleLangDropBtnClickEvent(dropContent) {
+  let items = dropContent.getElementsByClassName("lang-item");
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active-lang");
+      current[0].className = current[0].className.replace(" active-lang", "");
+      this.className += " active-lang";
+      dropContent.classList.remove("show");
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+import { HandleThemeDropBtnClickEvent } from "./dropdown.js";
+import { HandleLangDropBtnClickEvent } from "./dropdown.js";
+
 const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   lineNumbers: true,
   mode: "javascript",
   theme: "dracula",
 });
-
+const langDropBtn = document.querySelector(".js-lang-dropbtn");
+const themeDropBtn = document.querySelector(".js-theme-dropbtn");
+const langDropContent = document.querySelector(".js-lang-dropdown-content");
+const themeDropContent = document.querySelector(".js-theme-dropdown-content");
 const colorPicker = document.querySelector(".js-color-picker");
 const SnippetBackground = document.querySelector(
   ".js-export-container-wrapper"
@@ -11,6 +17,15 @@ const SnippetBackground = document.querySelector(
 
 colorPicker.addEventListener("input", (e) => {
   changeSnippetBackgroundColorOnChange();
+});
+
+themeDropBtn.addEventListener("click", function () {
+  document.getElementById("themeDropdown").classList.toggle("show");
+  HandleThemeDropBtnClickEvent(themeDropContent);
+});
+langDropBtn.addEventListener("click", function () {
+  document.getElementById("langDropdown").classList.toggle("show");
+  HandleLangDropBtnClickEvent(langDropContent);
 });
 
 function changeSnippetBackgroundColorOnChange() {
