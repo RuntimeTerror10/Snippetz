21dfef14b2c885901921a9619a9e20490238a05d
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-10 17:14:30 +0530

    snippet background color from color picker

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const test = document.querySelector("#editor");
-var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
   lineNumbers: true,
   mode: "javascript",
   theme: "dracula",
 });
+
+const colorPicker = document.querySelector(".js-color-picker");
+const SnippetBackground = document.querySelector(
+  ".js-export-container-wrapper"
+);
+
+colorPicker.addEventListener("input", (e) => {
+  changeSnippetBackgroundColorOnChange();
+});
+
+function changeSnippetBackgroundColorOnChange() {
+  const color = colorPicker.value;
+  SnippetBackground.style.background = color;
+}
