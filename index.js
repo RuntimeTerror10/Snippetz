7ecb4d6b59b2ffff7614c281c19c70ab95136594
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-10 10:16:16 +0530

    toggle theme and language dropdowns

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const test = document.querySelector("#editor");
-var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-  lineNumbers: true,
-  mode: "javascript",
-  theme: "dracula",
+const langDropBtn = document.querySelector(".js-lang-dropbtn");
+const themeDropBtn = document.querySelector(".js-theme-dropbtn");
+
+themeDropBtn.addEventListener("click", function () {
+  document.getElementById("themeDropdown").classList.toggle("show");
+});
+themeDropBtn.addEventListener("blur", function () {
+  let a = document.querySelector(".js-theme-dropdown-content");
+  a.classList.remove("show");
+});
+
+langDropBtn.addEventListener("click", function () {
+  document.getElementById("langDropdown").classList.toggle("show");
+});
+langDropBtn.addEventListener("blur", function () {
+  let a = document.querySelector(".js-lang-dropdown-content");
+  a.classList.remove("show");
 });
