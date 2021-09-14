ec9a04480b66f1694bd70281701ef0cbc3d75334
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-14 22:57:09 +0530

    saving snippet code and other parameters

---
+++ save.js
@@ -0,0 +1,9 @@
+export function saveSnippet(code, name, langName, langid) {
+  let key = name;
+  let obj = {
+    snippet: code,
+    language: langName,
+    languageid: langid,
+  };
+  window.localStorage.setItem(key, JSON.stringify(obj));
+}
