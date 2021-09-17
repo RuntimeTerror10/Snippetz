8e278a8e26dc1d2658127059b029414bcc085036
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-17 14:03:37 +0530

    show save msg on click

--- save.js
+++ save.js
@@ -1,3 +1,5 @@
+const saveBtn = document.querySelector(".js-save-btn");
+
 function saveSnippet(code, name, langName, langid) {
   let key = name;
   let obj = {
@@ -6,6 +8,13 @@ function saveSnippet(code, name, langName, langid) {
     languageid: langid,
   };
   window.localStorage.setItem(key, JSON.stringify(obj));
+  showSaveMsg();
+}
+function showSaveMsg() {
+  saveBtn.innerText = "Saved!";
+  setTimeout(function () {
+    saveBtn.innerText = "Save";
+  }, 800);
 }
 
 export default saveSnippet;
